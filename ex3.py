f2226762e924044b5880c2cc47df98d6f8f5bab5
allasultan31 <91071451+allasultan31@users.noreply.github.com>
2022-03-28 19:03:32 +0300

    Delete ex3.py

--- ex3.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-x = np.arange(-15, 15, 0.01)
-plt.plot(x, np.log((x*x+1)*np.exp(np.abs(x)/10)) / np.log(1+np.tan(1/(1+np.sin(x)*np.sin(x)))))
-plt.show()
